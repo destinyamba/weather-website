a4e1be51c896d04318a8e8ab57ae0a671d429c64
destiny.amba <destiny.ambakederemo@workrise.com>
2023-11-08 10:35:31 +0000

    deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/weather-website/',
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, 'src')}]
   },
-  base:'/dist',
 })
